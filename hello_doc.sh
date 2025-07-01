5f1f87a36b02d69803b464688886e4be9e9570e7
LeoDG <leonardodg@msn.com>
2025-07-01 16:25:49 +0100

    first commit

---
+++ hello_doc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# head_doc.sh: This script shows an example about how documentation my scripts.
+
+# Site: https://leonardodg.github.com/website
+# Author: LeoDG
+# Issue: ?
+
+# ----------------------------------------------------
+# This script print a simple message "Hello World" to the console.
+# The result output in default STDOUT.
+#
+# Exemple usage:
+#   $ ./head_doc.sh
+#   "Hello World"
+#
+# Short Execution:
+#   1. print "Hello World" to the console.
+#   2. exit with status 0
+# ----------------------------------------------------
+#
+# History:
+#   2025-21-06: Initial version
+#
+# License: MIT License
+
+echo "Hello World"
